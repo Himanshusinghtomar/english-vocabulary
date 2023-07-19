466b075c9a53485e8335af766b7866e31cd7dd27
Abhishek Kumar Raj <abhishekkumarraj62073@gmail.com>
2023-07-19 12:24:51 +0530

    change in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className=' bg-green-500'>
+        <h1 className=' text-center font-extrabold'>
+          Hello India
+        </h1>
+    </div>
   )
 }
 
